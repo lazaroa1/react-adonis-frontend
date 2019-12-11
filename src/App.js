e9d92f376676ea1f09b0c9ab74ebd0eee2975f9d
lazaroAM <44584192+lazaroAM@users.noreply.github.com>
2019-12-11 19:27:20 -0300

    frontend react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const api = axios.create({
+  baseURL: 'http://localhost:3333'
+})
+
+export default class src extends Component {
+  state= {
+    newPostContent: '',
+    posts: [],
+  }
+  
+  async componentDidMount() {
+    const {data: posts} = await api.get('/posts')
+
+    this.setState({posts})
+  }
+  
+  handleDelete = async (id) => {
+    await api.delete(`/posts/${id}`)
+    
+    this.setState({posts: this.state.posts.filter(item => item.id !== id)})
+  }
+
+  handlePostSave = async (e) => {
+    e.preventDefault()
+
+    const {data: post} = await api.post('/posts', {content: this.state.newPostContent})
+
+    this.setState({posts: [...this.state.posts, post], newPostContent: ''})
+  }
+
+  
+  render() {
+    return <div className="App">
+      <form onSubmit={this.handlePostSave}>
+        <textarea 
+          onChange={e => this.setState({newPostContent: e.target.value})}
+          value={this.state.newPostContent}
+        />
+        <button type="submit">Postar</button>
+      </form>
+
+      <ul>
+        {this.state.posts.map(post => (
+          <li onClick={() => this.handleDelete(post.id)} key={post.id}>{post.content}</li>
+        ))}
+      </ul>
     </div>
-  );
+  }
 }
 
-export default App;
